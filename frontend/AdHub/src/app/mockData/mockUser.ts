5f96b1c7fcab52a00d078d8926277cf08def1852
Etienne <tiennebrb@gmail.com>
2019-11-06 12:54:38 -0500

    Changed mock data to fit UML

--- frontend/AdHub/src/app/mockData/mockUser.ts
+++ frontend/AdHub/src/app/mockData/mockUser.ts
@@ -3,7 +3,10 @@ import {User} from '../models/models';
 const mockUser = {
   name: 'John doe',
   email: 'john.doe@email.com',
-  id: '1234567890'
+  id: '1234567890',
+  company: 'Big Falafel inc',
+  address: '1888 Cote-des-Neiges',
+  adItems: []
 } as User;
 
 export default mockUser;
--- frontend/AdHub/src/app/models/models.ts
+++ frontend/AdHub/src/app/models/models.ts
@@ -18,4 +18,7 @@ export interface User {
   name: String;
   id: String;
   email: String;
+  company: String;
+  address: String;
+  adItems: AdItem[];
 }
