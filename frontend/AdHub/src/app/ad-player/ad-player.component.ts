337de29c904619f3345f31894a54239ce59b5e0a
Etienne <tiennebrb@gmail.com>
2019-11-06 14:13:22 -0500

    Worked on viewing pictures

--- frontend/AdHub/src/app/ad-player/ad-player.component.ts
+++ frontend/AdHub/src/app/ad-player/ad-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-ad-player',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdPlayerComponent implements OnInit {
 
-  constructor() { }
+
+  @Input()
+  image_base_64: String;
+
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this._snackBar.open('Press ESC to stop playing add', '', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition:'top',
+      panelClass :'center'
+    });
   }
+
+
 
 }
