397d854bdfe78dca1e2120f37c77387e75a0cbc9
Etienne Berube <tiennebrb@gmail.com>
2019-11-26 15:54:31 -0500

    Changed dashboard
    removed Active and inactive ads and created a pipe

---
+++ frontend/AdHub/src/app/pipes/active.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {AdItem} from '../models/models';
+
+@Pipe({
+  name: 'isActiveAd',
+  pure: false
+})
+export class ActivePipe implements PipeTransform {
+  transform(items: AdItem[], filter: string): any {
+    if (!items) {
+      return items;
+    }
+    // filter items array, items which match and return true will be
+    // kept, false will be filtered out
+    return items.filter(item => {
+      if(filter === 'inactive') {
+        return !item.active;
+      }else{
+        return item.active;
+      }
+    });
+  }
+}
--- frontend/AdHub/src/app/services/advertisement.service.ts
+++ frontend/AdHub/src/app/services/advertisement.service.ts
@@ -1,15 +1,50 @@
 import { Injectable } from '@angular/core';
 import mockAds from '../mockData/mockAds';
 import {AdItem} from '../models/models';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {host} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdvertisementService {
+  private readonly VIEW_ADS_PATH = '/data/';
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
-  getAdvertisement(): AdItem[]{
+  getMyAdvertisement(): AdItem[]{
     return mockAds;
+  }
+
+  uploadAd(name: string, category: String, locations: string[], image_64: string){
+    const body = {
+      name: name,
+      category: category,
+      locations: locations,
+      image_64: image_64
+    };
+    return this.http.post(host + this.VIEW_ADS_PATH, body);
+  }
+
+  getNextAdvertisement(excluded: string[], numberToBuffer: Number): Observable<AdItem[]>{
+
+    const params =  new HttpParams()
+    .append('excluded', excluded.join(','))
+    .set('numberToBuffer', numberToBuffer.toString());
+
+   return this.http.get<any>(host + this.VIEW_ADS_PATH, {params: params});
+  }
+
+  deleteAd(id: string){
+    const params = new HttpParams().set('imageId', id);
+    return this.http.delete(host + this.VIEW_ADS_PATH, {params: params});
+  }
+
+  sendConfirmation(imageId: string){
+    this.http.put(host + this.VIEW_ADS_PATH, {imageId: imageId}).subscribe(()=>{},()=>{
+      console.log('Error when sending confirmation for: ' + imageId);
+    });
   }
 }
