383bdcec57620488661d7127e86c84ba3ee29979
Etienne <tiennebrb@gmail.com>
2019-11-27 13:32:18 -0500

    Added integration with backend

--- frontend/AdHub/src/app/services/advertisement.service.ts
+++ frontend/AdHub/src/app/services/advertisement.service.ts
@@ -4,13 +4,22 @@ import {AdItem} from '../models/models';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {host} from '../../environments/environment';
 import {Observable} from 'rxjs';
+import { interval } from 'rxjs';
 import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdvertisementService {
-  private readonly VIEW_ADS_PATH = '/data/';
+  private readonly PATH = '/data/';
+  private readonly CONFIG_PATH = 'config';
+  private readonly ADD_AD = 'add';
+  private readonly NEXT = 'next';
+  private readonly GET_MY_ADS = 'ads';
+  private readonly DELETE_AD = 'DELETE';
+  private readonly CHANGE_ACTIVE_AD = '';
+
+  private periodicSubscriber: Observable<any>;
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
@@ -21,30 +30,45 @@ export class AdvertisementService {
   uploadAd(name: string, category: String, locations: string[], image_64: string){
     const body = {
       name: name,
+      user_id: this.authService.getMyId(),
+      region: locations,
+      upload_date: Date(),
       category: category,
-      locations: locations,
       image_64: image_64
     };
-    return this.http.post(host + this.VIEW_ADS_PATH, body);
-  }
-
-  getNextAdvertisement(excluded: string[], numberToBuffer: Number): Observable<AdItem[]>{
-
-    const params =  new HttpParams()
-    .append('excluded', excluded.join(','))
-    .set('numberToBuffer', numberToBuffer.toString());
-
-   return this.http.get<any>(host + this.VIEW_ADS_PATH, {params: params});
+    return this.http.post(host + this.PATH + this.ADD_AD, body);
   }
 
   deleteAd(id: string){
-    const params = new HttpParams().set('imageId', id);
-    return this.http.delete(host + this.VIEW_ADS_PATH, {params: params});
+    const params = new HttpParams()
+      .set('image_id', id)
+      .set('user_id', this.authService.getMyId());
+    return this.http.delete(host + this.PATH + this.DELETE_AD, {params: params});
   }
 
-  sendConfirmation(imageId: string){
-    this.http.put(host + this.VIEW_ADS_PATH, {imageId: imageId}).subscribe(()=>{},()=>{
-      console.log('Error when sending confirmation for: ' + imageId);
-    });
+  sendConfiguration(included: string[]){
+    return this.http.post(host + this.PATH + this.CONFIG_PATH, included);
   }
+
+  startStreaming(): Observable<any>{
+     this.periodicSubscriber = interval(30000);
+
+     return this.periodicSubscriber;
+  }
+
+  getNextAdd(lastAdSeen: string){
+    const params = new HttpParams()
+      .set('last_ad_id', lastAdSeen)
+      .set('user_id', this.authService.getMyId());
+
+    return this.http.get(host + this.PATH + this.NEXT, {params: params});
+  }
+
+  getMyAds(){
+    const params = new HttpParams()
+      .set('user_id', this.authService.getMyId());
+
+    return this.http.get(host + this.PATH + this.GET_MY_ADS, {params: params});
+  }
+
 }
